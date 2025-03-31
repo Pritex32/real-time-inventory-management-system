56c01b411c3e20fa82f4fae174a4e9d6655a5a48
Prisca Ukanwa <oluomachiukanwa32@gmail.com>
2025-03-31 00:25:53 -0700

    Update All_Inventory.py

--- inventory_dashboard/pages/All_Inventory.py
+++ inventory_dashboard/pages/All_Inventory.py
@@ -1,7 +1,7 @@
 from http import cookies
 import streamlit as st
 import pandas as pd
-import psycopg2
+
 from streamlit_option_menu import option_menu
 from datetime import datetime,timedelta ,date
 from streamlit_cookies_manager import EncryptedCookieManager
